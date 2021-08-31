428e2814a7361c1d9cf66db58338411de8a72d71
Tharani Harichandran <tharani.harichandran@aspiresys.com>
2021-08-31 19:33:34 +0530

    index file

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include "includes/header.php";
+?>
+<body>
+<div class="scrollmenu">
+  <a href="http://localhost/leave_Management_System">Home</a>
+  <a href="http://localhost/leave_Management_System/view/employee/employee_login.php">Employee Login</a>
+
+</div>
+<div class="welcome">
+<h2><?php echo "Welcome To Employee Leave Management System"?> </h2>
+<h4><?php echo "All the employees can apply for a leave here and the admin can approve or remove the request of leave" ?> </h4>
+<?php
+include "includes/footer.php";
+?>
