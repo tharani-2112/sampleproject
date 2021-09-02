c54b27af8974cf84f78da3ab2121eecb6f1c49c5
Tharani Harichandran <tharani.harichandran@aspiresys.com>
2021-09-02 14:35:55 +0530

    setup file

--- setup/db.php
+++ setup/db.php
@@ -1,16 +1,27 @@
 <?php
+$databaseHost = 'localhost';
+$databaseName = 'leave_management';
+$dbUsername = 'root';
+$dbPassword = '';    
+$mysqli = new mysqli($databaseHost, $dbUsername, $dbPassword, $databaseName); 
+if(!$mysqli)
+{
+    die(mysqli_error($mysqli));
+}  
 
-use leave_management;
+$sql1="CREATE TABLE IF NOT EXISTS `admin`( id int,
+username varchar(100),
+password varchar(100),
+PRIMARY KEY (id)
+)";
+$result1=mysqli_query($mysqli,$sql1);
 
-CREATE TABLE admin( id int,
-                    username varchar(100),
-                    password varchar(100),
-                    PRIMARY KEY (id)
-);
-INSERT INTO `admin` (`id`, `UserName`, `Password`) VALUES
-(1, 'admin', 'Test@123');
 
-CREATE TABLE IF NOT EXISTS `tblemployees` (
+$sql2="INSERT INTO `admin` (`id`, `UserName`, `Password`) VALUES
+(1, 'admin', 'Test@123')";
+$result2=mysqli_query($mysqli,$sql2);
+
+$sql3="CREATE TABLE IF NOT EXISTS `tblemployees` (
     `id` int(11) NOT NULL,
     `firstName` varchar(150) NOT NULL,
     `lastName` varchar(150) NOT NULL,
@@ -22,18 +33,20 @@ CREATE TABLE IF NOT EXISTS `tblemployees` (
     `country` varchar(150) NOT NULL,
     `address` varchar(255) NOT NULL,
     `RegDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-  ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1";
+  $result3=mysqli_query($mysqli,$sql3);
 
 
-  CREATE TABLE IF NOT EXISTS `tblleavetype` (
+  $sql4="CREATE TABLE IF NOT EXISTS `tblleavetype` (
     `id` int(11) NOT NULL,
     `leavetype` varchar(200) DEFAULT NULL,
     `description` mediumtext,
     `CreationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-  ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1";
+  $result4=mysqli_query($mysqli,$sql4);
   
 
-  CREATE TABLE IF NOT EXISTS `tblleaves` ( 
+  $sql5="CREATE TABLE IF NOT EXISTS `tblleaves` ( 
     `lid` int(11) NOT NULL, 
     `name` varchar(110) NOT NULL,
     `email` varchar(110) NOT NULL,
@@ -44,13 +57,15 @@ CREATE TABLE IF NOT EXISTS `tblemployees` (
     `PostingDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, 
     `AdminRemark` mediumtext, 
     `Status` int(1) NOT NULL
-  ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+  ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1";
+  $result5=mysqli_query($mysqli,$sql5);
 
-  CREATE TABLE IF NOT EXISTS `manageremployee` (
+  $sql6="CREATE TABLE IF NOT EXISTS `manageremployee` (
     `id` int(11) NOT NULL,
     `manager` int(11) NOT NULL,
     `employee`int(11) NOT NULL ,
     `CreationDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-  ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1";
+  $result6=mysqli_query($mysqli,$sql6);
   
 ?>
